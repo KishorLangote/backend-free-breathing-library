1f8c03a5c41912741fd63154e6279de46efdbb73
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-18 19:07:13 +0530

    chore: Updates cors.

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const { initializeDatabase } = require("./db/db.connect")
 const app = express();
 
 const corsOptions = {
-  origin: "*" ,              // this allows request from any origin
+  origin: "*",
+  method: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  allowedHeaders: "Content-Type, Authorization",              // this allows request from any origin
   credentials: true,        // this allows cookies, authorization headers, TLS certi..
   optionSuccessStatus: 200, // set status code 200 fir successful request..
 }
