7f108dd376f295e721e0b47e61e5477c489a4a30
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-18 15:52:23 +0530

    chore uncomment code

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,3 +1,28 @@
+// const jwt = require("jsonwebtoken")
+
+// const authenticateToken = (req, res, next) => {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1]; 
+  
+//   if(token == null) {
+//     return res.status(401).json({ message: "Authentication token requried "})
+//   }
+
+//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//     if(err){
+//       return res.status(403).json({ message: "Token expired. Please signIn again" })
+//     }
+//     console.log("Decoded Token:", user); // Debugging: See if user data is there
+//     req.user = user;   // Attach decoded user data (id, name, role) to req.user
+//     next()
+//   })
+// }
+
+// module.exports = { authenticateToken }
+
+
+
+
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
