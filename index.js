7717a6558589aac88aaed12465c06ba8df64f84c
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-19 06:00:10 +0530

    chore: Updates cors.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const corsOptions = {
   origin: "*",
   method: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  allowedHeaders: "Content-Type, Authorization",              // this allows request from any origin
+  allowedHeaders: "Content-Type, authorization",              // this allows request from any origin
   credentials: true,        // this allows cookies, authorization headers, TLS certi..
   optionSuccessStatus: 200, // set status code 200 fir successful request..
 }
