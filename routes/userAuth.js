a615927735c0e43cf32695abb613b8abd17c34a1
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-19 06:08:24 +0530

    Updates user auth file.

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,44 +1,44 @@
-// const jwt = require("jsonwebtoken")
-
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1]; 
-  
-//   if(token == null) {
-//     return res.status(401).json({ message: "Authentication token requried "})
-//   }
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if(err){
-//       return res.status(403).json({ message: "Token expired. Please signIn again" })
-//     }
-//     console.log("Decoded Token:", user); // Debugging: See if user data is there
-//     req.user = user;   // Attach decoded user data (id, name, role) to req.user
-//     next()
-//   })
-// }
-
-// module.exports = { authenticateToken }
-
-
-
-
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1]; // Extract token from header
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; 
+  
+  if(token == null) {
+    return res.status(401).json({ message: "Authentication token requried "})
+  }
 
-    if (!token) {
-        return res.status(401).json({ message: "Access Denied. No token provided!" });
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if(err){
+      return res.status(403).json({ message: "Token expired. Please signIn again" })
     }
+    console.log("Decoded Token:", user); // Debugging: See if user data is there
+    req.user = user;   // Attach decoded user data (id, name, role) to req.user
+    next()
+  })
+}
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // verify token
-        req.user = decoded; // Attach decoded token payload (which should include userId) to req.user
-        next();
-    } catch (error) {
-        res.status(403).json({ message: "Invalid or expired token!" });
-    }
-};
+module.exports = { authenticateToken }
 
-module.exports = {authenticateToken};
+
+
+
+// const jwt = require("jsonwebtoken");
+
+// const authenticateToken = (req, res, next) => {
+//     const token = req.headers.authorization?.split(" ")[1]; // Extract token from header
+
+//     if (!token) {
+//         return res.status(401).json({ message: "Access Denied. No token provided!" });
+//     }
+
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET); // verify token
+//         req.user = decoded; // Attach decoded token payload (which should include userId) to req.user
+//         next();
+//     } catch (error) {
+//         res.status(403).json({ message: "Invalid or expired token!" });
+//     }
+// };
+
+// module.exports = {authenticateToken};
