836bf2ee07da5de798fbb4483b5213e50037c005
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-21 12:39:39 +0530

    Chore: Updates user auth

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,28 +1,3 @@
-// const jwt = require("jsonwebtoken")
-
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-
-//   console.log("Authorization Header:", authHeader); 
-
-//   const token = authHeader && authHeader.split(" ")[1]; 
-  
-//   if(token == null) {
-//     return res.status(401).json({ message: "Authentication token required" })
-//   }
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if(err){
-//       return res.status(403).json({ message: "Token expired. Please signIn again" })
-//     }
-//     console.log("Decoded Token:", user); // if user data is there
-//     req.user = user;   // decoded user data (id, name, role) to req.user
-//     next()
-//   })
-// }
-
-// module.exports = { authenticateToken }
-
 
 const jwt = require("jsonwebtoken");
 
